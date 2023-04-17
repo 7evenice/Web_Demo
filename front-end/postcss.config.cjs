e9a32885f4afef5bdde1a63e56db2985a26bd56e
7777 <vermas8900@gmail.com>
2023-04-17 14:39:25 +0700

    checking the config postcss & tailwindcss

--- front-end/postcss.config.cjs
+++ front-end/postcss.config.cjs
@@ -1,9 +1,6 @@
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("postcss-preset-env")({ stage: 1 }),
-    require("cssnano")({
-      preset: "default"
-    })
-  ]
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
