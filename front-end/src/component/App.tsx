f032f57a5b2c37e0944c323e2d5a567c239fa342
7777 <vermas8900@gmail.com>
2023-04-19 15:44:11 +0700

    check Login 1

--- front-end/src/component/App.tsx
+++ front-end/src/component/App.tsx
@@ -2,12 +2,23 @@ import { useState } from 'react'
 import '../App.css'
 import '../index.css'
 import Login from './Login'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const App = (): JSX.Element => {
+  const router = createBrowserRouter([
+    // {
+    //   path: '/',
+    //   element: <Home />
+    // },
+    {
+      path: '/login',
+      element: <Login />
+    }
+  ])
 
   return (
-    <div className="App">
-      <Login/>
+    <div className='w-full h-screen'>
+      <RouterProvider router={router} />
     </div>
   )
 }
